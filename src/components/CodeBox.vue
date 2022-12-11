1ca47023713f34c146e6a40d95ea169a6cabfa40
Donya <thisis2nya@gmail.com>
2022-12-11 19:45:13 +0330

    feat: Reset code

---
+++ src/components/CodeBox.vue
@@ -0,0 +1,5 @@
+<template>
+	<p
+		type="number"
+		class="code-digit border-2 border-emerald-600 rounded-lg mx-1 w-10 h-10 flex items-center justify-center cursor-text"></p>
+</template>
--- src/components/CodeContainer.vue
+++ src/components/CodeContainer.vue
@@ -1,19 +1,67 @@
 <template>
 	<div class="flex items-center gap-4">
-		<CodeInput />
-		<CodeInput />
-		<CodeInput />
-		<CodeInput />
-		<CodeInput />
-		<CodeInput />
+		<input
+			id="code"
+			type="number"
+			@input="changeInput($event)"
+			ref="registerCode"
+			class="border-2 mx-auto my-6 opacity-0 w-0 h-0"
+			maxlength="6" />
+		<label for="code" class="flex justify-center items-end gap-1.5 my-4 ltr">
+			<codeBox />
+			<codeBox />
+			<codeBox />
+			<codeBox />
+			<codeBox />
+			<codeBox />
+			<img
+				src="../assets/icon/trash.svg"
+				@click="resetCode"
+				class="w-4 mb-1 cursor-pointer"
+				alt="Trash icon" />
+		</label>
 	</div>
 </template>
 
 <script>
-import CodeInput from "./CodeInput.vue";
+import { ref } from "vue";
+import codeBox from "./CodeBox.vue";
 export default {
 	components: {
-		CodeInput,
+		codeBox,
+	},
+	setup() {
+		const registerCode = ref(null);
+		function changeInput(e) {
+			const codeBoxes = document.querySelectorAll(".code-digit");
+			const code = e.target.value;
+			for (let i in code) {
+				const letter = code[i];
+				if (codeBoxes[i]) {
+					codeBoxes[i].innerText = letter;
+				}
+			}
+		}
+		function resetCode() {
+			registerCode.value.value = "";
+			let codeBoxes = document.querySelectorAll(".code-digit");
+			codeBoxes = Array.prototype.slice.call(codeBoxes);
+			codeBoxes.forEach((box) => {
+				box.innerText = "";
+			});
+			console.log(registerCode.value.value);
+		}
+		return {
+			changeInput,
+			resetCode,
+			registerCode,
+		};
 	},
 };
 </script>
+
+<style scoped>
+label {
+	direction: ltr;
+}
+</style>
